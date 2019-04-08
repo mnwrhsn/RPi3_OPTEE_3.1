8c899d6019d1c5291d0721092791d9ac7cbaadf4
Monowar Hasan <mnwr.hsn@gmail.com>
2019-04-08 16:48:37 -0400

    copy2sd

---
+++ copy_to_sdcard.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# This script will copy the contents to removable storage for
+# Raspberry pi
+
+# check lsblk output and make sure the device id, eg. /dev/sdbX
+
+PWD_CUR=$(pwd)
+
+DEV_BOOT="/dev/sdb1"
+DEV_ROOTFS="/dev/sdb2"
+
+OPTEE_ROOT="/home/mhasan/workspace/rpi_optee310"
+
+# mount filesystem
+
+echo "Mounting filesystem..."
+
+sudo mkdir /media/boot
+sudo mkdir /media/rootfs
+
+# sudo mount $DEV_BOOT /media/boot
+# sudo mount $DEV_ROOTFS /media/rootfs
+
+echo "Copy library and examples ..."
+
+# copy filesystme, library
+cd /media
+LOC="$OPTEE_ROOT/gen_rootfs/filesystem.cpio.gz"
+sudo gunzip -cd $LOC | sudo cpio -iudmv "boot/*"
+
+LOC="$OPTEE_ROOT/module_output/lib/*"
+sudo cp -r $LOC rootfs/lib/
+
+LOC="$OPTEE_ROOT/optee_client/out/export/*"
+sudo cp -r $LOC rootfs/
+
+# copy OPTEE examples
+LOC="$OPTEE_ROOT/optee_examples/out/ca/*"
+sudo cp -r $LOC  rootfs/bin/
+
+# copy TA files
+LOC="$OPTEE_ROOT/optee_examples/out/ta/*"
+sudo cp -r $LOC rootfs/lib/optee_armtz/
+
+echo "Unmounting filesystem..."
+
+# Unmount
+sudo umount /media/boot/
+sudo umount /media/rootfs/
+
+# echo $LOC
+# sudo rm -r /media/boot
+# sudo rm -r /media/rootfs
+
+echo "Script finished!"
