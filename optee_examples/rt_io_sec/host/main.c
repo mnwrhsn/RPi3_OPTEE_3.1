00fb25830c5ab696bf826c0498479e3991dffed3
Monowar Hasan <mnwr.hsn@gmail.com>
2019-04-17 16:48:42 -0400

    can make clinet as shared lib

--- optee_examples/rt_io_sec/host/main.c
+++ optee_examples/rt_io_sec/host/main.c
@@ -25,8 +25,18 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <err.h>
+
+// for shared library
+#define _GNU_SOURCE
+
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <dlfcn.h>
+
+// for OPTEE
+#include <err.h>
 #include <string.h>
 
 /* OP-TEE TEE client API (built by optee_client) */
@@ -35,72 +45,86 @@
 /* To the the UUID (found the the TA's h-file(s)) */
 #include <rt_io_ta.h>
 
-int main(int argc, char *argv[])
-{
-	TEEC_Result res;
-	TEEC_Context ctx;
-	TEEC_Session sess;
-	TEEC_Operation op;
-	TEEC_UUID uuid = TA_HELLO_WORLD_UUID;
-	uint32_t err_origin;
+// int main(int argc, char *argv[])
+// {
+// 	TEEC_Result res;
+// 	TEEC_Context ctx;
+// 	TEEC_Session sess;
+// 	TEEC_Operation op;
+// 	TEEC_UUID uuid = TA_HELLO_WORLD_UUID;
+// 	uint32_t err_origin;
+//
+// 	/* Initialize a context connecting us to the TEE */
+// 	res = TEEC_InitializeContext(NULL, &ctx);
+// 	if (res != TEEC_SUCCESS)
+// 		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
+//
+// 	/*
+// 	 * Open a session to the "hello world" TA, the TA will print "hello
+// 	 * world!" in the log when the session is created.
+// 	 */
+// 	res = TEEC_OpenSession(&ctx, &sess, &uuid,
+// 			       TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
+// 	if (res != TEEC_SUCCESS)
+// 		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",
+// 			res, err_origin);
+//
+// 	/*
+// 	 * Execute a function in the TA by invoking it, in this case
+// 	 * we're incrementing a number.
+// 	 *
+// 	 * The value of command ID part and how the parameters are
+// 	 * interpreted is part of the interface provided by the TA.
+// 	 */
+//
+// 	/* Clear the TEEC_Operation struct */
+// 	memset(&op, 0, sizeof(op));
+//
+// 	/*
+// 	 * Prepare the argument. Pass a value in the first parameter,
+// 	 * the remaining three parameters are unused.
+// 	 */
+// 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INOUT, TEEC_NONE,
+// 					 TEEC_NONE, TEEC_NONE);
+// 	op.params[0].value.a = 46;
+//
+// 	/*
+// 	 * TA_HELLO_WORLD_CMD_INC_VALUE is the actual function in the TA to be
+// 	 * called.
+// 	 */
+// 	printf("Invoking TA to increment %d\n", op.params[0].value.a);
+// 	res = TEEC_InvokeCommand(&sess, TA_HELLO_WORLD_CMD_INC_VALUE, &op,
+// 				 &err_origin);
+// 	if (res != TEEC_SUCCESS)
+// 		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
+// 			res, err_origin);
+// 	printf("TA incremented value to %d\n", op.params[0].value.a);
+//
+// 	/*
+// 	 * We're done with the TA, close the session and
+// 	 * destroy the context.
+// 	 *
+// 	 * The TA will print "Goodbye!" in the log when the
+// 	 * session is closed.
+// 	 */
+//
+// 	TEEC_CloseSession(&sess);
+//
+// 	TEEC_FinalizeContext(&ctx);
+//
+// 	return 0;
+// }
 
-	/* Initialize a context connecting us to the TEE */
-	res = TEEC_InitializeContext(NULL, &ctx);
-	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
+int i=0;
 
-	/*
-	 * Open a session to the "hello world" TA, the TA will print "hello
-	 * world!" in the log when the session is created.
-	 */
-	res = TEEC_OpenSession(&ctx, &sess, &uuid,
-			       TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
-	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",
-			res, err_origin);
+int ioctl(int d, unsigned long request, char *argp) {
 
-	/*
-	 * Execute a function in the TA by invoking it, in this case
-	 * we're incrementing a number.
-	 *
-	 * The value of command ID part and how the parameters are
-	 * interpreted is part of the interface provided by the TA.
-	 */
+	if (i==0) {
+       printf("My IOCTL CALL\n");
+       pid_t pid = syscall(SYS_getpid);
+       printf("PID from wrapper: %d\n", pid);
+       i+=1;
+   }
 
-	/* Clear the TEEC_Operation struct */
-	memset(&op, 0, sizeof(op));
-
-	/*
-	 * Prepare the argument. Pass a value in the first parameter,
-	 * the remaining three parameters are unused.
-	 */
-	op.paramTypes = TEEC_PARAM_TYPES(TEEC_VALUE_INOUT, TEEC_NONE,
-					 TEEC_NONE, TEEC_NONE);
-	op.params[0].value.a = 46;
-
-	/*
-	 * TA_HELLO_WORLD_CMD_INC_VALUE is the actual function in the TA to be
-	 * called.
-	 */
-	printf("Invoking TA to increment %d\n", op.params[0].value.a);
-	res = TEEC_InvokeCommand(&sess, TA_HELLO_WORLD_CMD_INC_VALUE, &op,
-				 &err_origin);
-	if (res != TEEC_SUCCESS)
-		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
-			res, err_origin);
-	printf("TA incremented value to %d\n", op.params[0].value.a);
-
-	/*
-	 * We're done with the TA, close the session and
-	 * destroy the context.
-	 *
-	 * The TA will print "Goodbye!" in the log when the
-	 * session is closed.
-	 */
-
-	TEEC_CloseSession(&sess);
-
-	TEEC_FinalizeContext(&ctx);
-
-	return 0;
+    return 0;
 }
